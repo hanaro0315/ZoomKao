2effcc0f727f9666bb0e828cf4702be455d40953
sangh <sangh@DESKTOP-1GKSBO5>
2021-04-23 17:58:35 +0900

    refs #day1 goodbye upload..

--- reHanaro2/src/main/java/com/example/demo/controller/MainController.java
+++ reHanaro2/src/main/java/com/example/demo/controller/MainController.java
@@ -1,13 +1,25 @@
 package com.example.demo.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+import com.example.demo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import com.example.demo.Dto.Member;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	private MemberService member;
 
 	@RequestMapping(value="/")
 	public String home() {
@@ -19,9 +31,25 @@ public class MainController {
 		return "login";
 	}
 	
-	@PostMapping(value="signUp")
+	@PostMapping(value="/signUp")
 	public String signUp() {
 		return "signUp";
 	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Member> getMember(@PathVariable("id") Long id){
+		Optional<Member> members = member.findById(id);
+		return new ResponseEntity<Member>(members.get(), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> deleteMember(@PathVariable("id") Long id){
+		member.deleteById(id);
+		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+	}
+	
+	@PostMapping(value="/{save}")
+	
+	
 	
 }
--- reHanaro2/src/main/java/com/example/demo/service/MemberService.java
+++ reHanaro2/src/main/java/com/example/demo/service/MemberService.java
@@ -1,9 +1,14 @@
 package com.example.demo.service;
 
 import java.util.ArrayList;
+import com.example.demo.Dto.Member;
 import java.util.List;
+import java.util.Optional;
+
 import com.example.demo.Dto.Member;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import com.example.demo.Repository.customRepository;
 
@@ -15,6 +20,22 @@ public class MemberService {
 	
 	public List<Member> findAll(){
 		List<Member> members = new ArrayList<>();
+		custom.findAll().forEach(e -> members.add(e));
 		return members;
 	}
+	
+	public Optional<Member> findById(Long id){
+		Optional<Member> member = custom.findById(id);
+		return member;
+	}
+	
+	public void deleteById(Long id) {
+		custom.deleteById(id);
+	}
+	
+	public Member save(Member member) {
+		custom.save(member);
+		return member;
+	}
+	
 }
